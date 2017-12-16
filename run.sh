4ca3f4dede4cf3cf74324dd43a8c9b685446214d
Pablo Picasso <dnovikov1808@gmail.com>
2017-12-16 04:29:58 +0300

    chore(scripts): update script 'run.sh'

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+sleep 1
+if [ "$(id -u)" != "0" ]; then
+    echo "\e[1mThis script must be run as root\e[0m"
+    exit 1
+else
+    sleep 1
+    echo "\e[1mInstall basic packages\e[0m"
+    sh ./scripts/basic.sh
+fi
+
+sleep 1
+exit 1
