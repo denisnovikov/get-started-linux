6a712009f535a7bc43c9ac76be61dfd189b140f3
Pablo Picasso <dnovikov1808@gmail.com>
2017-12-16 05:00:18 +0300

    chore(scripts): created script 'run-default.sh'

---
+++ run-default.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sleep 1
+if [ "$(id -u)" != "0" ]; then
+    sleep 1
+    echo "\e[1mCreating a file structure for front-end development\e[0m"
+    sh ./scripts/structure.sh
+else
+    echo "\e[1mThis script must be run as normal user\e[0m"
+    exit 1
+fi
+
+sleep 1
+exit 1
